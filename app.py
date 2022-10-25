bd02536675b816f7f25338a8601753eb280b79a6
Alelopez383 <43974872+Alelopez383@users.noreply.github.com>
2022-10-25 12:10:27 -0500

    Adding routes

--- app.py
+++ app.py
@@ -30,4 +30,17 @@ def welcome():
     /api/v1.0/temp/start/end
     ''')
 
-  
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
